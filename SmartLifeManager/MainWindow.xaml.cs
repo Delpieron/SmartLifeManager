8c74c754c9cdc6f5fb54a7b6f8ad600ca182173b
Krzysiek Wróblewski <krzysiuw1906@gmail.com>
2021-12-11 02:51:18 +0100

    dbadded

--- SmartLifeManager/MainWindow.xaml.cs
+++ SmartLifeManager/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using SmartLifeManager.Data;
+using SmartLifeManager.Models;
 using SmartLifeManager.Views;
 using System;
 using System.Windows;
@@ -17,15 +18,12 @@ namespace SmartLifeManager
         public MainWindow()
         {
             InitializeComponent();
-            //File.Create(@"D:\kupa.sqlite");
-            /*
-                        DBAllContext allContext = new DBAllContext("D:\\RatMonDB.RM2");
-                        allContext.CreateStructure("schedule");*/
 
-
+            DBAllContext allContext = new DBAllContext(AppDomain.CurrentDomain.BaseDirectory + "smartLifeDB.db");
+            var a = allContext.AddTreeElement(new ScheduleModel { Event = "event", RainfallSum = "rain", Wet = "wet", WindDirection = "direction", WindSpeed = "speed", Pressure = "pressure", Temperature = "temperature", ExecutionTime = DateTime.Now });
+            var b = allContext.GetElements();
             ChangeView(ViewType.WidgetList);
         }
-
 
         #region ViewManager
         BaseViewControl currentView = null;
