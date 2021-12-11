d3207ae517282607d0af05450c9b93eb499d901c
Krzysztof Domoń <krzysztof.domon@o2.pl>
2021-12-11 02:46:23 +0100

    water condition update

---
+++ SmartLifeManager/Models/Water.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartLifeManager.Models
+{
+    public class Water
+    {
+        public string StationId;
+        public string Station;
+        public string River;
+        public string WaterStatus;
+        public string StatusDateTime;
+        public string WaterTemperature;
+        public string TemperatureDateTime;
+        public string IcePhenomen;
+        public string IceDateTime;
+        public string FoulingPhenomenon;
+        public string FoulingDateTime;
+
+        public Water(string id_stacji, string stacja, string rzeka, string stan_wody, string stan_wody_data_pomiaru, string temperatura_wody, string temperatura_wody_data_pomiaru, string zjawisko_lodowe, string zjawisko_lodowe_data_pomiaru, string zjawisko_zarastania, string zjawisko_zarastania_data_pomiaru)
+        {
+            StationId = id_stacji;
+            River = rzeka;
+            Station = stacja;
+            WaterStatus = stan_wody;
+            StatusDateTime = stan_wody_data_pomiaru;
+            WaterTemperature = temperatura_wody;
+            TemperatureDateTime = temperatura_wody_data_pomiaru;
+            IcePhenomen = zjawisko_lodowe;
+            IceDateTime = zjawisko_lodowe_data_pomiaru;
+            FoulingPhenomenon = zjawisko_zarastania;
+            FoulingDateTime = zjawisko_zarastania_data_pomiaru;
+        }
+    }
+}
--- SmartLifeManager/Views/WeatherView.xaml.cs
+++ SmartLifeManager/Views/WeatherView.xaml.cs
@@ -3,6 +3,7 @@ using SmartLifeManager.Data;
 using SmartLifeManager.Models;
 using System.Net.Http;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Media;
 
 namespace SmartLifeManager.Views
@@ -23,40 +24,35 @@ namespace SmartLifeManager.Views
         {
             imageBox.Content = "\u2601";
             imageBox.Foreground = Brushes.DarkGray;
-            using (var client = new HttpClient())
+            try
             {
-                //using (var request = new HttpRequestMessage(HttpMethod.Get, "https://danepubliczne.imgw.pl/api/data/synop/station/" + city.ToLower()))
-                //{
-                //    using (var response = await client.SendAsync(request))
-                //    {
-                //        response.EnsureSuccessStatusCode();
-                //        var responseBody = await response.Content.ReadAsStringAsync();
-                //        Weather weather = JsonConvert.DeserializeObject<Weather>(responseBody);
+                using (var client = new HttpClient())
+                {
+                    using (var request = new HttpRequestMessage(HttpMethod.Get, "https://danepubliczne.imgw.pl/api/data/synop/station/" + city.ToLower()))
+                    {
+                        using (var response = await client.SendAsync(request))
+                        {
+                            response.EnsureSuccessStatusCode();
+                            var responseBody = await response.Content.ReadAsStringAsync();
+                            Weather weather = JsonConvert.DeserializeObject<Weather>(responseBody);
 
-                //        LocationLabel.Content += "\n" + city;
-                //        DateAndTimeLabel.Content = weather.Date + "  " + weather.Time + ":00";
-                //        TemperatureLabel.Content = weather.Temperature + " °C";
-                //        WindSpeedLabel.Content = weather.WindSpeed + " m/s";
-                //        WindDirectionLabel.Content = weather.WindDirection;
-                //        WetLabel.Content = weather.Wet + " g/m³";
-                //        TotalPrecipitationLabel.Content = weather.TotalPrecipitation + " mm/m²";
-                //        PressureLabel.Content = weather.Pressure + " hPa";
-                //    }
-                //}
-                var request = new HttpRequestMessage(HttpMethod.Get, "https://api.gios.gov.pl/pjp-api/rest/station/findAll");
-                var response = await client.SendAsync(request);
-                response.EnsureSuccessStatusCode();
-                var responseBody = await response.Content.ReadAsStringAsync();
+                            LocationLabel.Content += "\n" + city;
+                            DateAndTimeLabel.Content = weather.Date + "  " + weather.Time + ":00";
+                            TemperatureLabel.Content = weather.Temperature + " °C";
+                            WindSpeedLabel.Content = weather.WindSpeed + " m/s";
+                            WindDirectionLabel.Content = weather.WindDirection;
+                            WetLabel.Content = weather.Wet + " g/m³";
+                            TotalPrecipitationLabel.Content = weather.TotalPrecipitation + " mm/m²";
+                            PressureLabel.Content = weather.Pressure + " hPa";
+                        }
+                    }
+                }
+            }
+            catch (System.Exception)
+            {
+                MessageBox.Show("Connection error");
             }
 
-
-
-        }
-
-        private void test(object sender, System.Windows.Input.MouseButtonEventArgs e)
-        {
-            imageBox.Content = "\u2600";
-            imageBox.Foreground = Brushes.Yellow;
         }
     }
 }
